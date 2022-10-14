b6f4e3cee7ccedb673ff4461555e2fef2ddcedea
jjesusR <josedejesusramirezortega@gmail.com>
2022-10-14 02:39:10 -0500

    Commit 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Soy una PWA en React - Jose de Jesus</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
